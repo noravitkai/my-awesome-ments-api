023a40338140e98f5369c0c299ab21cc97c20218
noravitkai <norvit01@easv365.dk>
2025-01-31 08:30:39 +0100

    Server up and running with simple route

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from "express";
+import dotenvFlow from "dotenv-flow";
+import routes from "./routes";
+
+// dotenvFlow.config();
+
+const app: Application = express();
+
+app.use("/api", routes);
+
+export function startServer() {
+  app.listen(4000, function () {
+    console.log("Server is up and running on port 4000.");
+  });
+}
