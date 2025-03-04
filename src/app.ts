8c4d6dec65685ac08b326f2ce372ccbe296ec7bf
noravitkai <norvit01@easv365.dk>
2025-03-04 22:44:48 +0100

    Install CORS and configure middleware in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,32 @@ import express, { Application, Request, Response } from "express";
 import dotenvFlow from "dotenv-flow";
 import routes from "./routes";
 import { testConnection } from "./repository/database";
+import cors from "cors";
 
 dotenvFlow.config();
 
 const app: Application = express();
 
+function setupCors() {
+  app.use(
+    cors({
+      origin: "*",
+      methods: "GET,PUT,POST,DELETE",
+      allowedHeaders: [
+        "auth-token",
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+      ],
+      credentials: true,
+    })
+  );
+}
+
 export function startServer() {
+  setupCors();
+
   app.use(express.json());
 
   app.use("/api", routes);
